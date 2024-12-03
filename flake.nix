bfb20e88928ea52eb5541ae0256e8957c7d2a263
Thomas Laich <thomaslaich@gmail.com>
2024-12-03 23:10:37 +0100

    update, remove packages that are on nixpkgs-unstable, add kubectl-nvim

--- flake.nix
+++ flake.nix
@@ -8,22 +8,16 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    lsp-progress = {
-      type = "github";
-      owner = "linrongbin16";
-      repo = "lsp-progress.nvim";
-      flake = false;
-    };
-    startup = {
-      type = "github";
-      owner = "startup-nvim";
-      repo = "startup.nvim";
-      flake = false;
-    };
     org-bullets = {
       type = "github";
       owner = "nvim-orgmode";
       repo = "org-bullets.nvim";
+      flake = false;
+    };
+    kubectl = {
+      type = "github";
+      owner = "Ramilito";
+      repo = "kubectl.nvim";
       flake = false;
     };
   };
@@ -33,9 +27,8 @@
       self,
       nixpkgs,
       flake-utils,
-      lsp-progress,
-      startup,
       org-bullets,
+      kubectl,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -43,32 +36,24 @@
       let
         pkgs = import nixpkgs { inherit system; };
 
-        lsp-progress-nvim = pkgs.vimUtils.buildVimPlugin {
-          pname = "lsp-progress";
-          version = "2024-05-20";
-          src = lsp-progress;
-          meta.homepage = "https://github.com/linrongbin16/lsp-progress.nvim/";
-        };
-
-        startup-nvim = pkgs.vimUtils.buildVimPlugin {
-          pname = "startup";
-          version = "2023-12-20";
-          src = startup;
-          meta.homepage = "https://github.com/startup-nvim/startup.nvim";
-        };
-
         org-bullets-nvim = pkgs.vimUtils.buildVimPlugin {
           pname = "org-bullets";
-          version = "2024-02-21";
+          version = "2024-09-12";
           src = org-bullets;
           meta.homepage = "https://github.com/nvim-orgmode/org-bullets.nvim";
+        };
+
+        kubectl-nvim = pkgs.vimUtils.buildVimPlugin {
+          pname = "kubectl";
+          version = "2024-12-01";
+          src = kubectl;
+          meta.homepage = "https://github.com/Ramilito/kubectl.nvim";
         };
       in
       {
         packages = {
-          default = lsp-progress-nvim;
-          inherit lsp-progress-nvim;
-          inherit startup-nvim;
+          default = kubectl-nvim;
+          inherit kubectl-nvim;
           inherit org-bullets-nvim;
         };
       }
